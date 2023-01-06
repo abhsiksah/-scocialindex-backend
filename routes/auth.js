12347ffc90739f3ce1f03c0d82a3e5fffdd26814
abhsiksah <killuawpak@gmail.com>
2023-01-06 15:12:22 +0530

    v5 cleanup bugs

--- routes/auth.js
+++ routes/auth.js
@@ -6,23 +6,27 @@ const bcrypt = require("bcrypt");
 router.post("/register", async (req, res) => {
   try {
     //generate new password
-    console.log(req.body);
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
+    const duplicateuser = await User.findOne({ email: req.body.email });
+    console.log(duplicateuser);
+    duplicateuser && res.status(404).json("user already exists");
     //create new user
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPassword,
-      profilePicture: req.body.profilePicture,
-      coverPicture: req.body.coverPicture,
-      isAdmin: req.body.isAdmin,
-    });
 
-    //save user and respond
-    const user = await newUser.save();
-    res.status(200).json(user);
+    if (!duplicateuser) {
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+        profilePicture: req.body.profilePicture,
+        coverPicture: req.body.coverPicture,
+        isAdmin: req.body.isAdmin,
+      });
+
+      //save user and respond
+      const user = await newUser.save();
+      res.status(200).json(user);
+    }
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
